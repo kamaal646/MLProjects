5dac95aae8e8cc58957cd8d0b08f901fe8616e0c
kamaal646 <123kamaalkhan@gmail.com>
2024-12-26 10:21:30 +0530

    Hyperparameter Training

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import find_packages,setup
 from typing import List
 
-HYPHEN_E_DOT='-e .'
+HYPEN_E_DOT='-e .'
 def get_requirements(file_path:str)->List[str]:
     '''
-    This function will return the list of requirements
+    this function will return the list of requirements
     '''
-    
-    requirements = []
+    requirements=[]
     with open(file_path) as file_obj:
-        requirements = file_obj.readlines()
-        requirements = [req.replace("\n", "") for req in requirements]
-        
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
-            
-    return requirements
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
     
+    return requirements
 
 setup(
     name='mlproject',
@@ -26,6 +24,5 @@ setup(
     packages=find_packages(),
     # install_requires=['pandas','numpy','seaborn'] 
     
-    install_requires=get_requirements('requirements.txt')
-    
+    install_requires=get_requirements('requirements.txt')    
 )
